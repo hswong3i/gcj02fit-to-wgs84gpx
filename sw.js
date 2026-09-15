5d42a4164782983bde6b74bd228ff5250938d82d
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-09-15 15:31:55 +0800

    Offline Support
    Expanded Service Worker cache asset list to include essential vendor CSS
    files (`leaflet`, `bootstrap`, `bootstrap-icons`,
    `github-fork-ribbon-css`), ensuring a fully functional offline
    experience.

--- sw.js
+++ sw.js
@@ -6,10 +6,14 @@ const ASSETS = [
   './js/app.js',
   './favicon.svg',
   './node_modules/leaflet/dist/leaflet.js',
+  './node_modules/leaflet/dist/leaflet.css',
   './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  './node_modules/bootstrap/dist/css/bootstrap.min.css',
   './node_modules/chart.js/dist/chart.umd.js',
   './node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff2',
-  './node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff'
+  './node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff',
+  './node_modules/bootstrap-icons/font/bootstrap-icons.css',
+  './node_modules/github-fork-ribbon-css/gh-fork-ribbon.css'
 ];
 
 self.addEventListener('install', (event) => {
